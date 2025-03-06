e6ebf09df5badbe48dea08bbf71d34f7f0c8f9c9
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-06 09:29:27 +0100

    Update main_mixed.py

--- main_mixed.py
+++ main_mixed.py
@@ -36,4 +36,46 @@ print(f"Train tensor with force shape: {train_tensor_forced.shape}")       # Exp
 print(f"Test tensor shape: {test_tensor.shape}")          # Expected: [5000, 101, 3]
 print(f"Validation tensor shape: {val_tensor.shape}")     # Expected: [5000, 101, 3]
 
+# NN Structure
 
+Num_meas = 2
+Num_inputs = 1
+Num_x_Obsv = 3
+Num_u_Obsv = 3
+Num_x_Neurons = 30
+Num_u_Neurons = 30
+Num_hidden_x_encoder = 2
+Num_hidden_x_decoder = 2
+Num_hidden_u_encoder = 2
+Num_hidden_u_decoder = 2
+
+# Instantiate the model and move it to the GPU (if available)
+model = AUTOENCODER(Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder)
+
+
+# Training Loop
+start_training_time = time.time()
+
+eps = 2 # Number of epochs per batch size
+lr = 1e-3 # Learning rate
+batch_size = 256
+S_p = 30
+T = len(train_tensor_unforced[0, :, :])
+alpha = [0.1, 10e-7, 10e-15]
+W = 0
+M = 1 # Amount of models you want to run
+
+[Lowest_loss, Lowest_test_loss, Best_Model] = trainingfcn(eps, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder, train_tensor_unforced, train_tensor_forced, test_tensor, M)
+
+# Load the parameters of the best model
+model.load_state_dict(torch.load(Best_Model))
+print(f"Loaded model parameters from Model: {Best_Model}")
+
+end_time =  time.time()
+
+total_time = end_time - start_time
+total_training_time = end_time - start_training_time
+
+
+print(f"Total time is: {total_time}")
+print(f"Total training time is: {total_training_time}")
