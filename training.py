238c42ff11f40eff627689741b856be0a10c92d0
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-06 09:24:09 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,1 +1,10 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import torch.optim as optim
+import os
+
+from help_func import self_feeding, enc_self_feeding, set_requires_grad
+from loss_func import total_loss, total_loss_forced, total_loss_unforced
+from nn_structure import AUTOENCODER
+
 
