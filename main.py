682094971c19c11cf511a7e25d6b6947d176f27a
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-10 13:51:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from nn_structure import AUTOENCODER
 from training import trainingfcn
 from data_generation import DataGenerator
 from debug_func import debug_L12, debug_L3, debug_L4, debug_L5, debug_L6
-from plotting import plot_results
+from plotting import plot_results, plot_losses, plot_debug
 
 # Set device to GPU if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -63,7 +63,7 @@ alpha = [0.1, 10e-7, 10e-15]
 W = 0
 M = 2 # Amount of models you want to run
 
-[Lowest_loss, Lowest_test_loss, Best_Model] = trainingfcn(eps, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder, train_tensor, test_tensor, M)
+[Lowest_loss,Models_loss_list, Best_Model, Lowest_loss_index, Running_Losses_Array, Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array] = trainingfcn(eps, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder, train_tensor, test_tensor, M, device=None)
 
 # Load the parameters of the best model
 model.load_state_dict(torch.load(Best_Model, weights_only=True))
@@ -79,4 +79,6 @@ print(f"Total training time is: {total_training_time}")
 
 # Result Plotting
 
+plot_losses(Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array, Lowest_loss_index)
+plot_debug(model, val_tensor, train_tensor, S_p, Num_meas, Num_x_Obsv, T)
 plot_results(model, val_tensor, train_tensor, S_p, Num_meas, Num_x_Obsv, T)
