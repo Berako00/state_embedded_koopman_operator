2b978fcd4125f60f2a7bec47e17f937acee373a9
staskoko <117083501+staskoko@users.noreply.github.com>
2025-03-17 13:31:18 +0100

    Update ga_main.py

--- ga_main.py
+++ ga_main.py
@@ -9,8 +9,8 @@ import time
 
 from help_func import self_feeding, enc_self_feeding
 from nn_structure import AUTOENCODER
-from training import trainingfcn, trainingfcn_mixed
-from data_generation import DataGenerator, DataGenerator_mixed
+from training import trainingfcn
+from data_generation import DataGenerator
 
 from plotting import plot_results
 from ga_optimizer import run_genetic_algorithm
@@ -20,43 +20,50 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
 start_time = time.time()
-# Data Generation
 
+# ---- System Params ----------
+Num_meas = 4
+Num_inputs = 2
+system = 'two_link'     # 'two_link' or 'simple'
+# ----------------------------
+
+# ------- Data Generation Params ----------
 numICs = 10000
-x1range = (-0.5, 0.5)
-x2range = x1range
 T_step = 50
 dt = 0.02
-mu = -0.05
-lam = -1
 seed = 1
-generations = 5
-pop_size = 10
-eps = 500
-patience = 5
-Num_meas = 2
-Num_inputs = 1
 
-# Define training type
-training_type = 'normal' # 'mixed' or 'normal' training
+if system == 'simple':
+  x1range = (-0.5, 0.5)
+  x2range = x1range
+  mu = -0.05
+  lam = -1
+elif system == 'two_link':
+  q1_range = (-math.pi/2, math.pi/2)
+  q2_range = (-313.2/2*math.pi/180, 313.2/2*math.pi/180)
+  dq1_range = (-1, 1)
+  dq2_range = dq1_range
+  tau_max = 1
+# -----------------------------------------
 
-[train_tensor, test_tensor, val_tensor] = DataGenerator(x1range, x2range, numICs, mu, lam, T_step, dt)
+if system == 'simple':
+  [train_tensor, test_tensor, val_tensor] = DataGenerator(x1range, x2range, numICs, mu, lam, T_step, dt)
 
-[train_tensor_unforced, train_tensor_forced, test_tensor_unforced, test_tensor_forced, val_tensor] = DataGenerator_mixed(x1range, x2range, numICs, mu, lam, T_step, dt)
+elif system == 'two_link':
+  [train_tensor, test_tensor, val_tensor] = TwoLinkRobotDataGenerator(q1_range, q2_range, dq1_range, dq2_range, numICs, T_step, dt, tau_max)
 
-print(f"Train tensor shape: {train_tensor.shape}")
-print(f"Test tensor shape: {test_tensor.shape}")
-print(f"Validation tensor shape: {val_tensor.shape}")
+# ---- GA Params -------------
+use_ga = True
+generations = 1
+pop_size = 1
+eps = 10
+tournament_size = 3
+mutation_rate = 0.1
 
-print(f"Train tensor unforced shape: {train_tensor_unforced.shape}")
-print(f"Test tensor unforced shape: {test_tensor_unforced.shape}")
-print(f"Train tensor forced shape: {train_tensor_forced.shape}")
-print(f"Test tensor forced shape: {test_tensor_forced.shape}")
-
-# Define parameter ranges
+# Define parameter ranges For GA
 param_ranges = {
-    "Num_x_Obsv": (1, 5),
-    "Num_u_Obsv": (1, 5),
+    "Num_x_Obsv": (4, 20),
+    "Num_u_Obsv": (2, 20),
     "Num_x_Neurons": (10, 50),
     "Num_u_Neurons": (10, 50),
     "Num_hidden_x": (1, 3),  # Shared for both x encoder and decoder
@@ -65,12 +72,38 @@ param_ranges = {
     "alpha1": (1e-9, 1e-5),
     "alpha2": (1e-18, 1e-12)
 }
+# ------------------------------
+
+# ---- Define last training param -------
+eps_final = 10       # Number of epochs for final training
+lr = 1e-3       # Learning rate
+batch_size = 256
+S_p = 30
+T = len(train_tensor[0, :, :])
+W = 0
+M = 1  # Amount of models you want to run
+
+if not use_ga:
+    Num_x_Obsv    = 3
+    Num_u_Obsv    = 2
+    Num_x_Neurons = 30
+    Num_u_Neurons = 30
+    Num_hidden_x  = 2
+    Num_hidden_u  = 2
+    alpha         = [0.1, 10e-7, 10e-15]
+# ---------------------------------------
+
+
+print(f"Train tensor shape: {train_tensor.shape}")
+print(f"Test tensor shape: {test_tensor.shape}")
+print(f"Validation tensor shape: {val_tensor.shape}")
+
+
 
 # --- Genetic Algorithm Hyperparameter Optimization ---
-use_ga = True
 if use_ga:
     # For speed, use a lower number of epochs for evaluation (eps) and fewer generations/population size.
-    best_params = run_genetic_algorithm(Num_meas, Num_inputs, training_type, train_tensor, test_tensor, train_tensor_unforced, train_tensor_forced, test_tensor_unforced, test_tensor_forced, generations, pop_size, eps, param_ranges=param_ranges, elitism_count=1, patience=patience)
+    best_params = run_genetic_algorithm(Num_meas, Num_inputs, train_tensor, test_tensor, tournament_size, mutation_rate, generations, pop_size, eps, param_ranges=param_ranges, elitism_count=1)
 
     Num_meas      = best_params['Num_meas']
     Num_inputs    = best_params['Num_inputs']
@@ -82,17 +115,8 @@ if use_ga:
     Num_hidden_x  = best_params['Num_hidden_x']
     Num_hidden_u  = best_params['Num_hidden_u']
     alpha         = [best_params['alpha0'], best_params['alpha1'], best_params['alpha2']]
-else:
-    # Default hyperparameters
-    Num_meas      = 2
-    Num_inputs    = 1
-    Num_x_Obsv    = 3
-    Num_u_Obsv    = 2
-    Num_x_Neurons = 30
-    Num_u_Neurons = 30
-    Num_hidden_x  = 2
-    Num_hidden_u  = 2
-    alpha         = [0.1, 10e-7, 10e-15]
+
+
 
 model = AUTOENCODER(Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons,
                     Num_u_Obsv, Num_u_Neurons, Num_hidden_x,
@@ -101,30 +125,11 @@ model = AUTOENCODER(Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons,
 # Training Loop Parameters
 start_training_time = time.time()
 
-eps = 5000       # Number of epochs for final training
-lr = 1e-3       # Learning rate
-batch_size = 256
-S_p = 30
-T = len(train_tensor[0, :, :])
-W = 0
-M = 1  # Amount of models you want to run
 
-if training_type == 'normal':
-  [Lowest_loss,Models_loss_list, Best_Model, Lowest_loss_index, Running_Losses_Array, Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array] = trainingfcn(eps, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x, Num_hidden_x, Num_hidden_u, Num_hidden_u, train_tensor, test_tensor, M, device=None)
-
-elif training_type == 'mixed':
-  [Lowest_loss, Models_loss_list, Best_Model, Lowest_loss_index,
-  Running_Losses_Array, Lgx_unforced_Array, Lgu_forced_Array,
-  L3_unforced_Array, L4_unforced_Array, L5_unforced_Array, L6_unforced_Array,
-  L3_forced_Array, L4_forced_Array, L5_forced_Array, L6_forced_Array] = trainingfcn_mixed(eps, lr, batch_size, S_p, T, alpha,
-                                                                          Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons,
-                                                                          Num_u_Obsv, Num_u_Neurons, Num_hidden_x,
-                                                                          Num_hidden_x, Num_hidden_u, Num_hidden_u,
-                                                                          train_tensor_unforced, train_tensor_forced, test_tensor_unforced,
-                                                                          test_tensor_forced, M)
+[Lowest_loss, Models_loss_list, Best_Model, Lowest_loss_index, Running_Losses_Array, Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array] = trainingfcn(eps_final, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x, Num_hidden_x, Num_hidden_u, Num_hidden_u, train_tensor, test_tensor, M, device=None)
 
 # Load the parameters of the best model
-model.load_state_dict(torch.load(Best_Model, map_location=device, weights_only=True))
+model.load_state_dict(torch.load(Best_Model))
 print(f"Loaded model parameters from Model: {Best_Model}")
 
 end_time = time.time()
@@ -135,8 +140,4 @@ print(f"Total time is: {total_time}")
 print(f"Total training time is: {total_training_time}")
 
 # ----- Result Plotting and Further Analysis -----
-if training_type == 'normal':
-  plot_results(model, val_tensor, train_tensor, S_p, Num_meas, Num_x_Obsv, T)
-
-elif training_type == 'mixed':
-  plot_results(model, val_tensor, train_tensor_forced, S_p, Num_meas, Num_x_Obsv, T)
+plot_results(model, val_tensor, train_tensor, S_p, Num_meas, Num_x_Obsv, T)
