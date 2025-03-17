43a4393ed618a147be0fb5545e29119c76fbbfe7
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-17 15:29:00 +0100

    Update gen_ex.bat

--- gen_ex.bat
+++ gen_ex.bat
@@ -1,1 +1,16 @@
+@echo off
+pip install pyinstaller
+pyinstaller --onefile --windowed GUI_main.py
 
+:: Wait for PyInstaller to finish
+timeout /t 5 /nobreak >nul
+
+:: Set paths
+set "SOURCE=%CD%\dist\WAP.exe"
+set "DESKTOP=%USERPROFILE%\Desktop"
+set "SHORTCUT=%DESKTOP%\WAP.lnk"
+
+:: Create shortcut on the Desktop
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%SHORTCUT%'); $s.TargetPath='%SOURCE%'; $s.Save()"
+
+echo Shortcut created on Desktop!
