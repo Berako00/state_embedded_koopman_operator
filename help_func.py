99d50db3999a51d4c3a5d0648b54d9546e4ad3e9
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-04-02 15:01:46 +0200

    Update help_func.py

--- help_func.py
+++ help_func.py
@@ -5,15 +5,17 @@ import os
 from loss_func import custom_loss
 
 def get_model_path(i):
-    windows_dir = r"C:\Users\jokin\Desktop\Uni\Aalborg\Master\Masters_Thesis"
+    windows_dir1 = r"C:\Users\jokin\Desktop\Uni\Aalborg\Master\Masters_Thesis"
+    windows_dir2 = r"C:\C:\Users\Labuser\Desktop\WAPNN"
     linux_dir   = "/home/trarity/master/koopman_control/data"
     colab_dir1  = "/content/drive/My Drive/Colab Notebooks"
-    colab_dir2  = "/content/drive/MyDrive/Colab Notebooks"
-
-    path2 = os.path.join(windows_dir, f"Autoencoder_model_params{i}.pth")
+    colab_dir2  = "/content/drive/MyDrive/Colab Notebooks"  
+    
     path1 = os.path.join(linux_dir, f"Autoencoder_model_params{i}.pth")
+    path2 = os.path.join(windows_dir1, f"Autoencoder_model_params{i}.pth")
     path3 = os.path.join(colab_dir1, f"Autoencoder_model_params{i}.pth")
     path4 = os.path.join(colab_dir2, f"Autoencoder_model_params{i}.pth")
+    path5 = os.path.join(windows_dir2, f"Autoencoder_model_params{i}.pth")
 
     if os.path.exists(path1):
         chosen_path = path1
@@ -21,8 +23,10 @@ def get_model_path(i):
         chosen_path = path2
     elif os.path.exists(path3):
         chosen_path = path3
+    elif os.path.exists(path4):
+        chosen_path = path4    
     else:
-        chosen_path = path4
+        chosen_path = path5
     print("Using model path:", chosen_path)
     return chosen_path
 
